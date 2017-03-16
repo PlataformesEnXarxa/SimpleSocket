54fb3e93d4fe4a0bbc22f03044a3ebd1ad7f0f83
Chaimae El Morabet Lachkar <elmchai401@gmail.com>
2017-03-16 12:18:35 +0100

    Part obligatoria

--- app/src/main/java/network/manning/msi/com/SimpleSocket.java
+++ app/src/main/java/network/manning/msi/com/SimpleSocket.java
@@ -2,7 +2,8 @@ package network.manning.msi.com;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,98 +21,161 @@ import java.net.Socket;
 
 /**
  * Android direct to Socket example.
- *
+ * <p>
  * For this to work you need a server listening on the IP address and port specified. See the
  * NetworkSocketServer project for an example.
  *
- *
  * @author charliecollins
- *
  */
 public class SimpleSocket extends Activity {
 
     private static final String CLASSTAG = SimpleSocket.class.getSimpleName();
-
+    private static final String SERVER_IP = "192.168.1.2";
+    private Handler h;
     private EditText ipAddress;
     private EditText port;
     private EditText socketInput;
     private TextView socketOutput;
 
-    //private static final String SERVER_IP = "10.0.2.2";
 
     @Override
     public void onCreate(final Bundle icicle) {
-        Button socketButton;
-
         super.onCreate(icicle);
         this.setContentView(R.layout.simple_socket);
+
+        Button socketMessageButton;
+        Button socketRunnableButton;
 
         this.ipAddress = (EditText) findViewById(R.id.socket_ip);
         this.port = (EditText) findViewById(R.id.socket_port);
         this.socketInput = (EditText) findViewById(R.id.socket_input);
         this.socketOutput = (TextView) findViewById(R.id.socket_output);
-        socketButton = (Button) findViewById(R.id.socket_button);
+        socketMessageButton = (Button) findViewById(R.id.socket_message_button);
+        socketRunnableButton = (Button) findViewById(R.id.socket_runnable_button);
 
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.
-                Builder().permitNetwork().build());
+//        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
 
-        socketButton.setOnClickListener(new OnClickListener() {
+        h = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 0:
+                        socketOutput.append("Message -> " + msg.obj + '\n');
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+        socketMessageButton.setOnClickListener(new OnClickListener() {
 
             public void onClick(final View v) {
-                socketOutput.setText("");
+//                socketOutput.setText("");
                 try {
-                    //InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-                    InetAddress serverAddr = InetAddress.getByName(ipAddress.getText().toString());
-                    //String output = callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString());
-                    String output = callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString());
-                    socketOutput.setText(output);
+                    InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+//                    InetAddress serverAddr = InetAddress.getByName(ipAddress.getText().toString());
+//                    String output = callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString());
+                    callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString(), Type.MESSAGE);
+//                    socketOutput.setText(output);
                 } catch (java.net.UnknownHostException e1) {
                     e1.printStackTrace();
                 }
             }
         });
+
+        socketRunnableButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                socketOutput.setText("");
+                try {
+                    InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+//                    InetAddress serverAddr = InetAddress.getByName(ipAddress.getText().toString());
+//                    String output = callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString());
+                    callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString(), Type.RUNNABLE);
+//                    socketOutput.setText(output);
+                } catch (java.net.UnknownHostException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+
+        socketMessageButton.callOnClick();
     }
 
-    private String callSocket(final InetAddress ad, final String port, final String socketData) {
+    private void callSocket(final InetAddress ad, final String port, final String socketData, final Type type) {
 
-        Socket socket = null;
-        BufferedWriter writer = null;
-        BufferedReader reader = null;
-        String output = null;
 
-        try {
-            socket = new Socket(ad, Integer.parseInt(port));
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            // send input terminated with \n
-            String input = socketData;
-            writer.write(input + "\n", 0, input.length() + 1);
-            writer.flush();
-            // read back output
-            output = reader.readLine();
-            Log.d(getText(R.string.log_tag).toString(), " " + SimpleSocket.CLASSTAG + " output - " + output);
-            // send EXIT and close
-            writer.write("EXIT\n", 0, 5);
-            writer.flush();
-        } catch (IOException e) {
-            Log.e(getText(R.string.log_tag).toString(), " " + SimpleSocket.CLASSTAG + " IOException calling socket", e);
-        } finally {
-            try {
-                if (writer!=null)
-                  writer.close();
-            } catch (IOException e) { // swallow
+        new Thread() {
+            @Override
+            public void run() {
+                Socket socket = null;
+                BufferedWriter writer = null;
+                BufferedReader reader = null;
+                String output;
+                try {
+                    socket = new Socket(ad, Integer.parseInt(port));
+                    writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    // send input terminated with \n
+                    String input = socketData;
+                    writer.write(input + "\n", 0, input.length() + 1);
+                    writer.flush();
+                    // read back output
+                    output = reader.readLine();
+                    switch (type) {
+                        case MESSAGE:
+                            Message message = new Message();
+                            message.what = 0;
+                            message.obj = output;
+                            h.sendMessage(message);
+                            break;
+                        case RUNNABLE:
+                            h.post(new updateUIThread(output));
+                            break;
+                    }
+                    Log.d(getText(R.string.log_tag).toString(), " " + SimpleSocket.CLASSTAG + " output - " + output);
+                    // send EXIT and close
+                    writer.write("EXIT\n", 0, 5);
+                    writer.flush();
+                } catch (IOException e) {
+                    Log.e(getText(R.string.log_tag).toString(), " " + SimpleSocket.CLASSTAG + " IOException calling socket", e);
+                } finally {
+                    try {
+                        if (writer != null)
+                            writer.close();
+                    } catch (IOException e) { // swallow
+                    }
+                    try {
+                        if (reader != null)
+                            reader.close();
+                    } catch (IOException e) { // swallow
+                    }
+                    try {
+                        if (socket != null)
+                            socket.close();
+                    } catch (IOException e) { // swallow
+                    }
+                }
+//        return output;
             }
-            try {
-                if (reader!=null)
-                    reader.close();
-            } catch (IOException e) { // swallow
-            }
-            try {
-                if (socket!=null)
-                   socket.close();
-            } catch (IOException e) { // swallow
-            }
+        }.start();
+    }
+
+    private enum Type {
+        MESSAGE,
+        RUNNABLE
+    }
+
+    private class updateUIThread implements Runnable {
+        private final String output;
+
+        public updateUIThread(String output) {
+            this.output = output;
         }
-        return output;
+
+        @Override
+        public void run() {
+            socketOutput.append("Runnable -> " + output + '\n');
+        }
     }
 }
